bbdf9005defbb80f6690f66315888ca422ad610e
U61vashka <botyrbojey@gmail.com>
2022-06-23 15:28:47 +0600

    OpusEncoder bitrate support, use music signal

--- src/main/java/com/ubivashka/plasmovoice/audio/codecs/OpusCodecHolder.java
+++ src/main/java/com/ubivashka/plasmovoice/audio/codecs/OpusCodecHolder.java
@@ -1,18 +1,21 @@
 package com.ubivashka.plasmovoice.audio.codecs;
 
+import java.util.function.Supplier;
+
+import javax.sound.sampled.AudioFormat;
+
 import com.ubivashka.plasmovoice.function.MemoizingSupplier;
 import com.ubivashka.plasmovoice.opus.OpusDecoder;
 import com.ubivashka.plasmovoice.opus.OpusEncoder;
-import de.maxhenkel.opus4j.Opus;
 
-import javax.sound.sampled.AudioFormat;
-import java.util.function.Supplier;
+import de.maxhenkel.opus4j.Opus;
+import su.plo.voice.PlasmoVoice;
 
 public class OpusCodecHolder extends AbstractCodecHolder {
     private static final int MTU_SIZE = 1024;
-    private static final int JOPUS_MODE = Opus.OPUS_APPLICATION_VOIP;
+    private static final int JOPUS_MODE = Opus.OPUS_APPLICATION_AUDIO;
 
-    private Supplier<OpusEncoder> lazyEncoder = MemoizingSupplier.memoize(() -> new OpusEncoder(sampleRate, frameSize, MTU_SIZE, JOPUS_MODE));
+    private Supplier<OpusEncoder> lazyEncoder = MemoizingSupplier.memoize(() -> new OpusEncoder(sampleRate, frameSize, MTU_SIZE, JOPUS_MODE, 64000));
     private Supplier<OpusDecoder> lazyDecoder = MemoizingSupplier.memoize(() -> new OpusDecoder(sampleRate, frameSize, MTU_SIZE));
 
     public OpusEncoder getEncoder() {
@@ -49,7 +52,7 @@ public class OpusCodecHolder extends AbstractCodecHolder {
 
     @Override
     public void closeDecoder() {
-		getDecoder().close();
+        getDecoder().close();
     }
 
     @Override
@@ -58,8 +61,17 @@ public class OpusCodecHolder extends AbstractCodecHolder {
         this.audioFormat = new AudioFormat(sampleRate, 16, 1, true, false);
         this.frameSize = (sampleRate / 1000) * 2 * 20;
 
-        lazyEncoder = MemoizingSupplier.memoize(() -> new OpusEncoder(sampleRate, frameSize, MTU_SIZE, JOPUS_MODE));
+        lazyEncoder = MemoizingSupplier.memoize(() -> new OpusEncoder(sampleRate, frameSize, MTU_SIZE, JOPUS_MODE, 64000));
         lazyDecoder = MemoizingSupplier.memoize(() -> new OpusDecoder(sampleRate, frameSize, MTU_SIZE));
     }
 
+    public void setBitrate(int bitrate) {
+        lazyEncoder = MemoizingSupplier.memoize(() -> new OpusEncoder(sampleRate, frameSize, MTU_SIZE, JOPUS_MODE, bitrate));
+    }
+
+    public static OpusCodecHolder newCodecHolder() {
+        OpusCodecHolder codecHolder = new OpusCodecHolder();
+        codecHolder.setSampleRate(PlasmoVoice.getInstance().getVoiceConfig().getSampleRate());
+        return codecHolder;
+    }
 }
