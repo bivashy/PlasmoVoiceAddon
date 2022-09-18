bfadaae99e0a218b0c24a9e217036db2f7ca2994
U61vashka <botyrbojey@gmail.com>
2022-09-18 19:49:51 +0600

    Code beautify

--- src/main/java/com/ubivashka/plasmovoice/audio/player/controller/IPlasmoVoiceSoundController.java
+++ src/main/java/com/ubivashka/plasmovoice/audio/player/controller/IPlasmoVoiceSoundController.java
@@ -3,7 +3,7 @@ package com.ubivashka.plasmovoice.audio.player.controller;
 import com.ubivashka.plasmovoice.config.settings.MusicPlayerSettings;
 import com.ubivashka.plasmovoice.sound.ISoundFormat;
 
-public interface IPlasmoVoiceSoundController extends ISoundController{
+public interface IPlasmoVoiceSoundController extends ISoundController {
     MusicPlayerSettings getMusicPlayerSettings();
 
     int getDistance();
@@ -12,7 +12,7 @@ public interface IPlasmoVoiceSoundController extends ISoundController{
 
     IPlasmoVoiceSoundController setDistance(int distance);
 
-    static IPlasmoVoiceSoundController of(ISoundFormat soundFormat,int distance){
-        return new DefaultPlasmoVoiceSoundController(soundFormat,distance);
+    static IPlasmoVoiceSoundController of(ISoundFormat soundFormat, int distance) {
+        return new DefaultPlasmoVoiceSoundController(soundFormat, distance);
     }
 }
